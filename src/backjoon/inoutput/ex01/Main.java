a9242ad0df8a14a3cdafbddfe774ff89a03b0a0a
hyeonahsong <hyeonasong@naver.com>
2022-08-20 19:20:31 +0900

    First commit

---
+++ src/backjoon/inoutput/ex01/Main.java
@@ -0,0 +1,9 @@
+package backjoon.inoutput.ex01;
+
+//Hello World!를 출력하시오.
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    } // main
+} // class
