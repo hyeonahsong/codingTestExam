653673b7166241f50f7f432b9454c9aa28207d26
hyeonahsong <hyeonasong@naver.com>
2022-08-20 21:01:35 +0900

    입출력 7번 문제: 사칙연산

---
+++ src/backjoon/inoutput/ex07/Main.java
@@ -0,0 +1,34 @@
+package backjoon.inoutput.ex07;
+
+//두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+//입력
+//두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+//출력
+//첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+//예제 입력 1
+//7 3
+//예제 출력 1
+//10
+//4
+//21
+//2
+//1
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+
+        sc.close();
+    } // main
+} // class
