47f727f3200ef57cbc62432523e6da493899c0d2
hyeonahsong <hyeonasong@naver.com>
2022-08-20 19:42:10 +0900

    입출력 2번 문제도 정답!

--- src/backjoon/inoutput/ex01/Main.java
+++ src/backjoon/inoutput/ex01/Main.java
@@ -2,7 +2,8 @@ package backjoon.inoutput.ex01;
 
 //Hello World!를 출력하시오.
 
-class Main {
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     } // main
