82af21a004b089874a576815b1ad38eb3ba9a302
hyeonahsong <hyeonasong@naver.com>
2022-08-20 20:22:42 +0900

    입출력 4번 문제도 정답!

---
+++ src/backjoon/inoutput/ex04/Main.java
@@ -0,0 +1,20 @@
+package backjoon.inoutput.ex04;
+
+// 첫째 줄에 A-B를 출력한다.
+//예제 입력 1
+//3 2
+//예제 출력 1
+//1
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int A = sc.nextInt();
+int B = sc.nextInt();
+
+System.out.println(A-B);
+    } // main
+} // class
