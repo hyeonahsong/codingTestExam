452a0db5fdbb8d7f27282d9cc5294b500c39ead7
hyeonahsong <hyeonasong@naver.com>
2022-08-20 20:37:28 +0900

    입출력 5번 문제도 정답!

---
+++ src/backjoon/inoutput/ex05/Main.java
@@ -0,0 +1,30 @@
+package backjoon.inoutput.ex05;
+
+//두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+//입력
+//첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//출력
+//첫째 줄에 A×B를 출력한다.
+//예제 입력 1
+//1 2
+//예제 출력 1
+//2
+//예제 입력 2
+//3 4
+//예제 출력 2
+//12
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int A = sc.nextInt();
+       int B = sc.nextInt();
+
+       System.out.println(A*B);
+
+       sc.close();
+    } // main
+} // class
