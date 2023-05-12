1476a5ef8a70da00168bc223e426ad17831c4396
hasong <hasong@mtdata.co.kr>
2023-05-12 15:01:35 +0900

    6026번: 실수 2개를 입력받아 합 출력

--- python/codeup/convert_6025.py
+++ python/codeup/convert_6025.py
@@ -3,6 +3,16 @@
 if __name__ == '__main__':
 
     # 25. 정수 2개를 입력받아 합 출력(6025)
-    num_01, num_02 = input().split()
-    num_03 = int(num_01) + int(num_02)
+    # num_01, num_02 = input().split()
+    # num_03 = int(num_01) + int(num_02)
+    #
+    # print(num_03)
+
+
+
+    # 26. 실수 2개를 입력받아 합 출력(6026)
+    num_01 = input()
+    num_02 = input()
+    num_03 = float(num_01) + float(num_02)
+
     print(num_03)
