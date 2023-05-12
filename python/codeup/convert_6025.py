a2902d2b3a841fd515f09838d82572523dd07bfb
hasong <hasong@mtdata.co.kr>
2023-05-12 17:50:16 +0900

    6027번: 10진수를 입력받아 16진수(hexadecimal)로 출력

--- python/codeup/convert_6025.py
+++ python/codeup/convert_6025.py
@@ -11,8 +11,16 @@ if __name__ == '__main__':
 
 
     # 26. 실수 2개를 입력받아 합 출력(6026)
-    num_01 = input()
-    num_02 = input()
-    num_03 = float(num_01) + float(num_02)
+    # num_01 = input()
+    # num_02 = input()
+    # num_03 = float(num_01) + float(num_02)
+    #
+    # print(num_03)
 
-    print(num_03)
+
+
+    # 27. 10진수를 입력받아 16진수(hexadecimal)로 출력(6027)
+    num_01 = input()
+    num_01 = int(num_01)
+    print('%x' %num_01)
+
