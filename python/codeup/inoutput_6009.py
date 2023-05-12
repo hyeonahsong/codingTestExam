bf618bf200b4cbdac42221ff6c865dbbdea69bcc
hasong <hasong@mtdata.co.kr>
2023-05-12 11:27:27 +0900

    6012번: 두 개의 정수를 입력받아 줄 바꾸어 출력

--- python/codeup/inoutput_6009.py
+++ python/codeup/inoutput_6009.py
@@ -16,6 +16,18 @@ if __name__ == '__main__':
 
 
     # 11.실수 입력 받아 출력(6011)
-    num = input()
-    num = float(num)
-    print(num)
+    # num = input()
+    # num = float(num)
+    # print(num)
+
+
+
+    12. 두 개의 정수를 입력받아 줄 바꾸어 출력
+    num_01 = input()
+    num_02 = input()
+
+    num_01 = int(num_01)
+    num_02 = int(num_02)
+
+    print(num_01)
+    print(num_02)
