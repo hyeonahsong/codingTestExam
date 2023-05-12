d216884e77cdda6d22b1c72044e786d3231fcb5b
hasong <hasong@mtdata.co.kr>
2023-05-12 11:06:56 +0900

    6010번: 숫자를 입력받아 int형으로 출력

--- python/codeup/inoutput_6009.py
+++ python/codeup/inoutput_6009.py
@@ -3,5 +3,11 @@
 if __name__ == '__main__':
 
     # 9. 입력받은 문자 출력(6009)
-    char = input()
-    print(char)
+    # char = input()
+    # print(char)
+
+
+
+    # 10.숫자 입력받아 int형ㅡ로 출력
+    num = input()
+    num = int(num)
