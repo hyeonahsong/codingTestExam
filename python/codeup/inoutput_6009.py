ba7626b10c18eae42bcc057919e61b11ba4d6b59
hasong <hasong@mtdata.co.kr>
2023-05-12 13:20:30 +0900

    6024번: 알파벳 문자와 숫자로 이루어진 단어 2개를 입력받아 순서대로 이어 출력

--- python/codeup/inoutput_6009.py
+++ python/codeup/inoutput_6009.py
@@ -114,5 +114,13 @@ if __name__ == '__main__':
 
 
     # 23. '시:분:초' 형식으로 시간을 입력받아 분만 출력(6023)
-    time = input()
-    print(time.split(':')[1])
+    # time = input()
+    # print(time.split(':')[1])
+
+
+
+    # 24. 알파벳 문자와 숫자로 이루어진 단어 2개를 입력받아 순서대로 이어 출력(6024)
+    word_01, word_02 = input().split()
+    word = word_01 + word_02
+
+    print(word)
