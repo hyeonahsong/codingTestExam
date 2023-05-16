ec25438789031c234c131033fd0595ae14373081
hasong <hasong@mtdata.co.kr>
2023-05-16 13:26:02 +0900

    문제 유형 변화로 인해 파일 구분

---
+++ python/codeup/calculation_6032.py
@@ -0,0 +1,24 @@
+# 6032 ~
+
+if __name__ == '__main__':
+
+# 32. 입력된 정수의 부호를 바꾸어 출력(6032)
+# num_01 = int(input())
+# print(-num_01)
+
+
+# 33. 문자 1개를 입력받아 그 다음 문자를 출력(6033)
+# num_01 = input()
+# num_01 = ord(num_01) + 1
+#
+# print(chr(num_01))
+
+
+# 34. 정수 2개(a, b)를 입력받아 a에서 b를 뺀 차를 출력(6034)
+# a, b = input().split()
+# print(int(a) - int(b))
+
+
+# 35. 실수 2개(f1, f2)를 입력받아 곱을 출력(6035)
+f1, f2 = input().split()
+print(float(f1) * float(f2))
