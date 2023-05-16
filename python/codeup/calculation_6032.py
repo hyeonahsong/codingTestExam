af5c9cae2927d26bcc2831737dbaa04007b99345
hasong <hasong@mtdata.co.kr>
2023-05-16 14:08:26 +0900

    6041번: 정수 2개(a, b) 를 입력받아 a를 b로 나눈 나머지를 출력

--- python/codeup/calculation_6032.py
+++ python/codeup/calculation_6032.py
@@ -53,6 +53,12 @@ if __name__ == '__main__':
 
 
     # 40. 정수 2개(a, b) 를 입력받아 a를 b로 나눈 몫을 출력(6040)
+    #
+    # a, b = input().split()
+    # print(int(a) // int(b))
 
+
+
+    # 41. 정수 2개(a, b) 를 입력받아 a를 b로 나눈 나머지를 출력(6041)
     a, b = input().split()
-    print(int(a) // int(b))
+    print(int(a) % int(b))
