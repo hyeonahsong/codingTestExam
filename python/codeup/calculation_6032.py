25505b730695805c04c7882f588278c9f9f932d3
hyeonahsong <hyeonasong@naver.com>
2023-05-16 17:44:04 +0900

    6053번: 정수값이 입력될 때, 그 불 값을 반대로 출력

--- python/codeup/calculation_6032.py
+++ python/codeup/calculation_6032.py
@@ -133,5 +133,11 @@ if __name__ == '__main__':
 
 
     # 52. 정수가 입력되었을 때, True/False 로 평가(6052)
-    num_01 = int(input())
-    print(bool(num_01))
+    # num_01 = int(input())
+    # print(bool(num_01))
+
+
+
+    # 53. 정수값이 입력될 때, 그 불 값을 반대로 출력(6053)
+    num_01 = bool(int(input()))
+    print(not num_01)
