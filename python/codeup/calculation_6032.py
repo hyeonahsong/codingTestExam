aa252cc6a17b4010b940cde8f34e901bb4ae8b4f
hyeonahsong <hyeonasong@naver.com>
2023-05-16 18:20:51 +0900

    6058번: 2개의 정수값이 입력될 때, 그 불 값(True/False) 이 모두 False 일 때에만 True 를 출력

--- python/codeup/calculation_6032.py
+++ python/codeup/calculation_6032.py
@@ -166,8 +166,14 @@ if __name__ == '__main__':
 
 
     # 57. 2개의 정수값이 입력될 때, 그 불 값(True/False) 이 서로 같을 때에만 True 를 출력(6057)
-    num_01, num_02 = input().split()
-    a =bool(int(num_01)) and bool(int(num_02))
-    b = (not(bool(int(num_01)))) and not(bool(int(num_02)))
+    # num_01, num_02 = input().split()
+    # a =bool(int(num_01)) and bool(int(num_02))
+    # b = (not(bool(int(num_01)))) and not(bool(int(num_02)))
+    #
+    # print(a or b)
 
-    print(a or b)
+
+
+    # 58. 2개의 정수값이 입력될 때, 그 불 값(True/False) 이 모두 False 일 때에만 True 를 출력(6058)
+    num_01, num_02 = input().split()
+    print(not(bool(int(num_01))) and not(bool(int(num_02))))
