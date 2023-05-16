20308371cacfee2602d233b1424609965f9c8bc9
hyeonahsong <hyeonasong@naver.com>
2023-05-16 18:05:47 +0900

    6055번: 2개의 정수값이 입력될 때, 그 불 값이 하나라도 True 일 때에만 True 를 출력

--- python/codeup/calculation_6032.py
+++ python/codeup/calculation_6032.py
@@ -145,5 +145,11 @@ if __name__ == '__main__':
 
 
     # 54. 2개의 정수값이 입력될 때, 그 불 값이 모두 True 일 때에만 True 를 출력(6054)
+    # num_01, num_02 = input().split()
+    # print(bool(int(num_01)) and bool(int(num_02)))
+
+
+
+    # 55. 2개의 정수값이 입력될 때, 그 불 값이 하나라도 True 일 때에만 True 를 출력(6055)
     num_01, num_02 = input().split()
-    print(bool(int(num_01)) and bool(int(num_02)))
+    print(bool(int(num_01)) or bool(int(num_02)))
