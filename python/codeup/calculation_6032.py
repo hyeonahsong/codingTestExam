af6eab17cc8fa08de6e8f2786c688d6017e18ec9
hyeonahsong <hyeonasong@naver.com>
2023-05-16 13:54:24 +0900

    6039번: 실수 2개(f1, f2)를 입력받아 f1을 f2번 거듭제곱한 값을 출력

--- python/codeup/calculation_6032.py
+++ python/codeup/calculation_6032.py
@@ -40,5 +40,12 @@ if __name__ == '__main__':
 
 
     # 38. 정수 2개(a, b)를 입력받아 a를 b번 곱한 거듭제곱을 출력하는 프로그램을 작성해보자.(6038)
-    a, b = input().split()
-    print(int(a) ** int(b))
+    # a, b = input().split()
+    # print(int(a) ** int(b))
+
+
+
+    # 39. 실수 2개(f1, f2)를 입력받아 f1을 f2번 거듭제곱한 값을 출력(6039)
+    f1, f2 = input().split()
+
+    print(float(f1) ** float(f2))
