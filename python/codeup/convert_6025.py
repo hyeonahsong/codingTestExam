aaa16c8e6a5f9a482d62d42d6eaa81f46b5fa5f8
hyeonahsong <hyeonasong@naver.com>
2023-05-16 11:30:28 +0900

    6033번: 문자 1개를 입력받아 그 다음 문자를 출력

--- python/codeup/convert_6025.py
+++ python/codeup/convert_6025.py
@@ -66,5 +66,13 @@ if __name__ == '__main__':
 
 
     # 32. 입력된 정수의 부호를 바꾸어 출력(6032)
-    num_01 = int(input())
-    print(-num_01)
+    # num_01 = int(input())
+    # print(-num_01)
+
+
+
+    # 33. 문자 1개를 입력받아 그 다음 문자를 출력(6033)
+    num_01 = input()
+    num_01 = ord(num_01) + 1
+
+    print(chr(num_01))
