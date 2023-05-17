565b138c673d591c158f11992138649d6bbca14b
hyeonahsong <hyeonasong@naver.com>
2023-05-17 14:36:39 +0900

    6067번: 0이 아닌 정수 1개가 입력되었을 때 음수이면서 짝수이면, A, 음수이면서 홀수이면, B, 양수이면서 짝수이면, C, 양수이면서 홀수이면, D를 출력

--- python/codeup/condition_choice_6065.py
+++ python/codeup/condition_choice_6065.py
@@ -17,20 +17,39 @@ if __name__ == '__main__':
 
 
     # 66. 3개의 정수(a, b, c)가 입력되었을 때, 짝(even)/홀(odd)을 출력(6066)
-    a, b, c = input().split()
-    a, b, c = int(a), int(b), int(c)
+    # a, b, c = input().split()
+    # a, b, c = int(a), int(b), int(c)
+    #
+    # if a % 2:
+    #     print('odd')
+    # else:
+    #     print('even')
+    #
+    # if b % 2:
+    #     print('odd')
+    # else:
+    #     print('even')
+    #
+    # if c % 2:
+    #     print('odd')
+    # else:
+    #     print('even')
 
-    if a % 2:
-        print('odd')
-    else:
-        print('even')
 
-    if b % 2:
-        print('odd')
-    else:
-        print('even')
 
-    if c % 2:
-        print('odd')
-    else:
-        print('even')
+#     67. 0이 아닌 정수 1개가 입력되었을 때 음수이면서 짝수이면, A, 음수이면서 홀수이면, B, 양수이면서 짝수이면, C, 양수이면서 홀수이면, D
+# 를 출력(6067)
+    num_01 = input()
+    num_01 = int(num_01)
+
+    if (num_01 < 0) and (num_01 % 2 == 0):
+        print('A')
+
+    if (num_01 < 0 )and (num_01 % 2 != 0):
+        print('B')
+
+    if (num_01 > 0) and (num_01 % 2 == 0):
+        print('C')
+
+    if (num_01 > 0) and (num_01 % 2 != 0):
+        print('D')
