f72e537c7d8f99598645b8b975f6a1d9e97f85e8
hyeonahsong <hyeonasong@naver.com>
2023-05-17 17:53:19 +0900

    6078번: 영문 소문자 'q'가 입력될 때까지 입력한 문자를 계속 출력

--- python/codeup/synthesize_6077.py
+++ python/codeup/synthesize_6077.py
@@ -2,10 +2,19 @@
 
 if __name__ == '__main__':
     # 77. 정수(1 ~ 100) 1개를 입력받아 1부터 그 수까지 짝수의 합을 출력(7077)
-    num_01 = int(input())
-    s = 0
+    # num_01 = int(input())
+    # s = 0
+    #
+    # for i in range(0, num_01 + 1, 2):
+    #     s += i
+    #
+    # print(s)
 
-    for i in range(0, num_01 + 1, 2):
-        s += i
 
-    print(s)
+
+    # 78. 영문 소문자 'q'가 입력될 때까지 입력한 문자를 계속 출력(6078)
+    char_01 = 'a'
+
+    while char_01 != 'q':
+        char_01 = input()
+        print(char_01)
