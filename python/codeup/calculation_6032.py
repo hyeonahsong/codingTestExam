2f4d08c47d3dda0be24f1740a3f1bffb641de066
hasong <hasong@mtdata.co.kr>
2023-05-17 12:17:34 +0900

    6063번: 입력된 두 정수(a, b) 중 큰 값을 출력 단, 삼항연산자를 사용

--- python/codeup/calculation_6032.py
+++ python/codeup/calculation_6032.py
@@ -199,5 +199,14 @@ if __name__ == '__main__':
 
 
     # 62. 입력된 정수 두 개를 비트단위로 xor 연산한 후 그 결과를 정수로 출력(6062)
-    num_01, num_02 = input().split()
-    print(int(num_01) ^ int(num_02))
+    # num_01, num_02 = input().split()
+    # print(int(num_01) ^ int(num_02))
+
+
+
+    # 63. 입력된 두 정수(a, b) 중 큰 값을 출력 단, 삼항연산자를 사용(6063)
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+
+    print(a if a >= b else b)
