2bb38838475a48a73460e88e3a6ec5b33bb61f54
hyeonahsong <hyeonasong@naver.com>
2023-05-17 11:59:45 +0900

    6062번: 입력된 정수 두 개를 비트단위로 xor 연산한 후 그 결과를 정수로 출력

--- python/codeup/calculation_6032.py
+++ python/codeup/calculation_6032.py
@@ -193,5 +193,11 @@ if __name__ == '__main__':
 
 
     # 61. 입력된 정수 두 개를 비트단위로 or 연산한 후 그 결과를 정수로 출력(6061)
+    # num_01, num_02 = input().split()
+    # print(int(num_01) | int(num_02))
+
+
+
+    # 62. 입력된 정수 두 개를 비트단위로 xor 연산한 후 그 결과를 정수로 출력(6062)
     num_01, num_02 = input().split()
-    print(int(num_01) | int(num_02))
+    print(int(num_01) ^ int(num_02))
