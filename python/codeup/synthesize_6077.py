c75e061a60c482e76a65db137b368c42a1c0a7b5
hyeonahsong <hyeonasong@naver.com>
2023-05-22 10:42:37 +0900

    6079번: 1, 2, 3 ... 을 계속 더해 나갈 때, 그 합이 입력한 정수(0 ~ 1000)보다 같거나 작을 때까지만 계속 더하는 프로그램을 작성

--- python/codeup/synthesize_6077.py
+++ python/codeup/synthesize_6077.py
@@ -13,8 +13,21 @@ if __name__ == '__main__':
 
 
     # 78. 영문 소문자 'q'가 입력될 때까지 입력한 문자를 계속 출력(6078)
-    char_01 = 'a'
+    # char_01 = 'a'
+    #
+    # while char_01 != 'q':
+    #     char_01 = input()
+    #     print(char_01)
 
-    while char_01 != 'q':
-        char_01 = input()
-        print(char_01)
+
+
+    # 79. 1, 2, 3 ... 을 계속 더해 나갈 때, 그 합이 입력한 정수(0 ~ 1000)보다 같거나 작을 때까지만 계속 더하는 프로그램을 작성(6079)
+    num_01 = int(input())
+    s = 0
+
+    for i in range(1, num_01):
+        s = s + i
+
+        if num_01 <= s:
+            print(i)
+            break
