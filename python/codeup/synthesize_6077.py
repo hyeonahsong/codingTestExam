d93e87bb552342620cd7125580dc5d5dfac41d9e
hasong <hasong@mtdata.co.kr>
2023-05-22 16:55:56 +0900

    6082번: 1 부터 그 수까지 순서대로 공백을 두고 수를 출력하는데, 3 또는 6 또는 9가 포함 되어있는 수인 경우, 그 수 대신 영문 대문자 X 를 출력한다.

--- python/codeup/synthesize_6077.py
+++ python/codeup/synthesize_6077.py
@@ -46,7 +46,19 @@ if __name__ == '__main__':
 
 
 # 81. A, B, C, D, E, F 중 하나가 입력될 때, 1부터 F까지 곱한 16진수 구구단의 내용을 출력!(6081)
-    num_01 = int(input(), 16)
+#     num_01 = int(input(), 16)
+#
+#     for i in range(1,16):
+#         print('%X' % num_01, '*%X' % i, '=%X' % (num_01 * i), sep = '')
 
-    for i in range(1,16):
-        print('%X' % num_01, '*%X' % i, '=%X' % (num_01 * i), sep='')
+
+
+# 82. 1 부터 그 수까지 순서대로 공백을 두고 수를 출력하는데, 3 또는 6 또는 9가 포함 되어있는 수인 경우, 그 수 대신 영문 대문자 X 를 출력한다. (6082)
+    num_01 = int(input())
+
+    for i in range(1, num_01 + 1):
+        if (i % 10 == 3) or (i % 10 == 6) or (i % 10 == 9):
+            print('X', end = ' ')
+
+        else:
+            print(i, end = ' ')
