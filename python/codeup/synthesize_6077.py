9ad8af57137dc082ab0dbab8c1250782d6382d55
hasong <hasong@mtdata.co.kr>
2023-05-23 15:47:45 +0900

    6090번: 시작 값(a), 곱할 값(m), 더할 값(d), 몇 번째인지를 나타내는 정수(n)가 입력될 때, n번째 수를 출력하는 프로그램을 만들어보자.

--- python/codeup/synthesize_6077.py
+++ python/codeup/synthesize_6077.py
@@ -193,10 +193,21 @@ if __name__ == '__main__':
 
 
     # 89. 시작 값(a), 등비(r), 몇 번째인지를 나타내는 정수(n)가 입력될 때 n번째 수를 출력하는 프로그램을 만들어보자.(6089)
-    a, r, n = map(int, input().split())
-    m = a
+    # a, r, n = map(int, input().split())
+    # m = a
+    #
+    # for i in range(1, n):
+    #     m = m * r
+    #
+    # print(m)
+
+
+
+    # 90. 시작 값(a), 곱할 값(m), 더할 값(d), 몇 번째인지를 나타내는 정수(n)가 입력될 때, n번째 수를 출력하는 프로그램을 만들어보자. (6090)
+    a, m, d, n = map(int, input().split())
+    t = a
 
     for i in range(1, n):
-        m = m * r
+        t = t * m + d
 
-    print(m)
+    print(t)
