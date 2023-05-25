85e463c3b75468eb9e5ece2a775bf993c3d63c71
hyeonahsong <hyeonasong@naver.com>
2023-05-25 15:21:52 +0900

    6096번 오답 보관

--- python/codeup/list_6092.py
+++ python/codeup/list_6092.py
@@ -21,6 +21,8 @@ if __name__ == '__main__':
 
 
 
+
+
     # 93. 출석 번호를 n번 무작위로 불렀을 때, 부른 번호를 거꾸로 출력해 보자. (6093)
     # n = int(input())
     # num = input().split()
@@ -28,6 +30,8 @@ if __name__ == '__main__':
     #
     # for i in range(n-1, -1, -1):
     #     print(num_list[i], end = ' ')
+
+
 
 
 
@@ -50,25 +54,66 @@ if __name__ == '__main__':
 
 
 
+
+
     # 95. 바둑판(19 * 19)에 n개의 흰 돌을 놓는다고 할 때, n개의 흰 돌이 놓인 위치를 출력하는 프로그램을 작성해보자.(6095)
+    # n = int(input())
+    # list_01 = []
+    #
+    #
+    # for i in range(20):
+    #     list_01.append([])
+    #     for j in range(20):
+    #         list_01[i].append(0)
+    #
+    #
+    # for i in range(n):
+    #     x, y = map(int, input().split())
+    #     list_01[x][y] = 1
+    #
+    #
+    # for i in range(1, 20):
+    #     for j in range(1, 20):
+    #         print(list_01[i][j], end = ' ')
+    #     print()
+
+
+
+
+
+    # 96.바둑판(19 * 19)에 흰 돌(1) 또는 검정 돌(0)이 모두 꽉 채워져 놓여있을 때, n개의 좌표를 입력받아 십(+)자 뒤집기한 결과를 출력??(6096)
+    badukpan = []
+
+    for i in range(19):
+        badukpan.append([])
+        for j in range(19):
+            badukpan[i].append(0)
+
+
+    for i in range(19):
+        badukpan[i] = list(map(int, input().split()))
+
+
+
     n = int(input())
-    list_01 = []
-
-
-    for i in range(20):
-        list_01.append([])
-        for j in range(20):
-            list_01[i].append(0)
-
-
     for i in range(n):
         x, y = map(int, input().split())
-        list_01[x][y] = 1
-
-
-    for i in range(1, 20):
         for j in range(1, 20):
-            print(list_01[i][j], end = ' ')
-        print()
+            if badukpan[x][j] == 0:
+                badukpan[x][j] = 1
 
+            else:
+                badukpan[x][j] = 0
+
+            if badukpan[j][y] == 0:
+                badukpan[j][y] = 1
+
+            else:
+                badukpan[j][y] = 0
+
+
+    for i in range(19):
+        for j in range(19):
+            print(badukpan[i][j], end = ' ')
+        print()
 
