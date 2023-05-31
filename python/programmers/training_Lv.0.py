866f6632a752df6611e3753b7fccde11d5f508fe
hyeonahsong <hyeonasong@naver.com>
2023-05-31 17:56:37 +0900

    2번: 부분문자열

--- python/programmers/training_Lv.0.py
+++ python/programmers/training_Lv.0.py
@@ -2,16 +2,36 @@
 
 
 # 1. 문자열 정수의 합
-def solution(num_str):
-    list_01 = list(num_str)
-    sum = 0
+# def solution(num_str):
+#     list_01 = list(num_str)
+#     sum = 0
+#
+#     for i in range(len(list_01)):
+#         sum += int(list_01[i])
+#
+#     return sum
 
-    for i in range(len(list_01)):
-        sum += int(list_01[i])
 
-    return sum
+
+
+
+# 2. 부분문자열
+def solution(str1, str2):
+    if str1 in str2:
+        return 1
+
+    else:
+        return 0
+
+
+
 
 
 
 if __name__ == '__main__':
-    print(solution("123456789"))
+    # 1번
+    # print(solution("123456789"))
+
+    # 2번
+    print(solution("abc", "aabcc"))
+    print(solution("tbt", "tbbttb"))
