14ca691a6719dabf6e09c72bd92ca830c4152de9
hyeonahsong <hyeonasong@naver.com>
2023-05-31 14:44:27 +0900

    6098번: 미로 상자의 구조가 0(갈 수 있는 곳), 1(벽 또는 장애물)로 주어지고, 먹이가 2로 주어질 때, 성실한 개미의 이동 경로를 예상해보자. 개미는 (2, 2)에서 출발한다.

--- python/codeup/list_6092.py
+++ python/codeup/list_6092.py
@@ -129,33 +129,85 @@ if __name__ == '__main__':
 
 
     # 97. 격자판의 세로(h), 가로(w), 막대의 개수(n), 각 막대의 길이(l), 막대를 놓는 방향(d:가로는 0, 세로는 1)과
-# 막대를 놓는 막대의 가장 왼쪽 또는 위쪽의 위치(x, y)가 주어질 때, 격자판을 채운 막대의 모양을 출력(6097)
-    h, w = map(int, input().split())
+# # 막대를 놓는 막대의 가장 왼쪽 또는 위쪽의 위치(x, y)가 주어질 때, 격자판을 채운 막대의 모양을 출력(6097)
+#     h, w = map(int, input().split())
+#
+#     # 0이 들어간 판 배열 생성
+#     pan_list = []
+#     for i in range(h):
+#         pan_list.append([])
+#         for j in range(w):
+#             pan_list[i].append(0)
+#
+#
+#
+#     # 입력받은 정보에 맞게 막대 부분 1로 할당해주기
+#     n = int(input())
+#
+#     for i in range(n):
+#         l, d, x, y = map(int, input().split())
+#         for j in range(l):
+#             if d == 0:
+#                 pan_list[x - 1][y - 1 + j] = 1
+#             else:
+#                 pan_list[x - 1 + j][y - 1] = 1
+#
+#
+#
+#     # 출력
+#     for i in range(h):
+#         for j in range(w):
+#             print(pan_list[i][j], end = ' ')
+#         print()
 
-    # 0이 들어간 판 배열 생성
-    pan_list = []
-    for i in range(h):
-        pan_list.append([])
-        for j in range(w):
-            pan_list[i].append(0)
 
 
 
-    # 입력받은 정보에 맞게 막대 부분 1로 할당해주기
-    n = int(input())
 
-    for i in range(n):
-        l, d, x, y = map(int, input().split())
-        for j in range(l):
-            if d == 0:
-                pan_list[x - 1][y - 1 + j] = 1
-            else:
-                pan_list[x - 1 + j][y - 1] = 1
+# 98. 미로 상자의 구조가 0(갈 수 있는 곳), 1(벽 또는 장애물)로 주어지고, 먹이가 2로 주어질 때,
+# 성실한 개미의 이동 경로를 예상해보자. 개미는 (2, 2)에서 출발한다.(6098)
+    ants = []
+
+    for i in range(10):
+        ants.append([])
+        for j in range(10):
+            ants[i].append(0)
+
+
+
+    # 개미굴 상황 입력받아 배열에 넣기
+    for i in range(10):
+        ant = list(map(int, input().split()))
+        for j in range(10):
+            ants[i] = ant
+
+
+
+    # 상황에 맞게 이동 경로 표시하기
+    x, y = 1, 1
+
+    while True:
+        if ants[x][y] == 0:
+            ants[x][y] = 9
+
+        elif ants[x][y] == 2:
+            ants[x][y] = 9
+            break
+
+        if ants[x][y+1] == 1 and ants[x+1][y] == 1:
+            break
+
+        if ants[x][y+1] != 1:
+            y += 1
+
+
+        elif ants[x+1][y] != 1:
+            x += 1
 
 
 
     # 출력
-    for i in range(h):
-        for j in range(w):
-            print(pan_list[i][j], end = ' ')
+    for i in range(10):
+        for j in range(10):
+            print(ants[i][j], end = ' ')
         print()
