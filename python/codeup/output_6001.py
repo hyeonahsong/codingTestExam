3c216830f935a3b1389281990e5f92f8fb71525d
hyeonahsong <hyeonasong@naver.com>
2023-08-01 13:25:25 +0900

    출력 문제 누락된 코드 업로드

---
+++ python/codeup/output_6001.py
@@ -0,0 +1,45 @@
+# 6001~6008
+
+if __name__ == '__main__':
+
+    # 1. "Hello" 출력(6001번)
+    # print('Hello')
+
+
+
+    # 2. "Hello World" 출력(6002)
+    # print('Hello World')
+
+
+
+    # 3. "Hello World" enter값 넣어 출력(6003)
+    # print('Hello')
+    # print('World')
+
+    # print('Hello\nWorld')
+
+
+
+    # 4. ' 포함된 문자열 출력'(6004)
+    # print("'" 'Hello' "'")
+
+
+
+     # 5. " 포함된 문자열 출력(6005)
+    # print('"' 'Hello World' '"')
+
+
+
+# 6. 특수기호 포함된 문자열 출력(6006)
+#     print('"' '!@#$%^&*()' "'")
+#     print('\"!@#$%^&*()\'')
+
+
+
+# 7.파일 경로 출력 (6007)
+#         print('\"C:\\Download\\\'hello\'.py\"')
+
+
+
+    # 8. '\n'이 포함된 코드 문자열 출력
+    # print('print(\"Hello\\nWorld\")')
